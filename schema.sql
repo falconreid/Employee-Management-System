2c8f97ddefefb58b9aa0ebdd5acf22e0afea232d
Joel Terrell <falconreid@hotmail.com>
2020-09-13 17:04:00 -0600

    split seed and schema files

---
+++ schema.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+  dept_name VARCHAR
+    (30),
+  PRIMARY KEY
+    (id)
+);
+
+    CREATE TABLE roles
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+  title VARCHAR
+        (30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY
+        (department_id) REFERENCES department
+        (id),
+  PRIMARY KEY
+        (id)
+);
+
+        CREATE TABLE employee
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+  first_name VARCHAR
+            (30),
+  last_name VARCHAR
+            (30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY
+            (role_id) REFERENCES roles
+            (id),
+  FOREIGN KEY
+            (manager_id) REFERENCES employee
+            (role_id),
+  PRIMARY KEY
+            (id)
+);
