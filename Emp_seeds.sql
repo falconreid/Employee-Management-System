bbbc350febbb6a8d4ffb9811ef433a317b9a170d
Joel Terrell <falconreid@hotmail.com>
2020-09-15 15:08:54 -0600

    fixed view functions, updated schema, seed files

--- Emp_seeds.sql
+++ Emp_seeds.sql
@@ -10,29 +10,29 @@ VALUES
   ("Sales");
 
 INSERT INTO roles
-  (title, salary)
+  (title, salary, department_id)
 VALUES
-  ("Chief Executive Officer", 250000),
-  ("Chief Information Officer", 200000),
-  ("Lead Engineer", 120000),
-  ("Associate Engineer", 65000),
-  ("Lead Legal Counsel", 120000),
-  ("Associate Legal Counsel", 65000),
-  ("Lead Sales", 120000),
-  ("Associate Sales", 65000);
+  ("Chief Executive Officer", 250000, 0),
+  ("Chief Information Officer", 200000, 0),
+  ("Lead Engineer", 120000, 1),
+  ("Associate Engineer", 65000, 1),
+  ("Lead Legal Counsel", 120000, 2),
+  ("Associate Legal Counsel", 65000, 2),
+  ("Lead Sales", 120000, 3),
+  ("Associate Sales", 65000, 3);
 
 
 INSERT INTO employee
-  (first_name, last_name)
+  (first_name, last_name, role_id, manager_id)
 VALUES
-  ("Jon", "Wisenheimer"),
-  ("Beau", "Borland"),
-  ("Johnny", "Jingleheimer"),
-  ("Jenny", "Johnsonville"),
-  ("Sue", "Thepantsoff"),
-  ("Takemto", "Slaughterhause"),
-  ("Yuno", "Yu Wa Na Buy"),
-  ("Sir Close", "Alot");
+  ("Jon", "Wisenheimer", 1, NULL),
+  ("Beau", "Borland", 2, 1 ),
+  ("Johnny", "Jingleheimer", 3, 2),
+  ("Jenny", "Johnsonville", 4, 3),
+  ("Sue", "Thepantsoff",  5, 1),
+  ("Takemto", "Slaughterhause", 6, 5),
+  ("Yuno", "Yu Wa Na Buy", 7, 1),
+  ("Sir Close", "Alot", 8, 7);
 
 
 
--- schema.sql
+++ schema.sql
@@ -14,21 +14,21 @@ CREATE TABLE department
     CREATE TABLE roles
     (
         id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY(department_id) REFERENCES department(id),
-  PRIMARY KEY(id)
+		title VARCHAR(30),
+		salary DECIMAL,
+		department_id INT,
+		FOREIGN KEY(department_id) REFERENCES department(id),
+		PRIMARY KEY(id)
 );
 
 	CREATE TABLE employee
 	(
-            id INT NOT NULL AUTO_INCREMENT,
-            first_name VARCHAR(30),
-            last_name VARCHAR(30),
-            role_id INT,
-            manager_id INT,
-            FOREIGN KEY(role_id) REFERENCES roles(id),
-            FOREIGN KEY (manager_id) REFERENCES employee(id),
-            PRIMARY KEY (id)
+		id INT NOT NULL AUTO_INCREMENT,
+		first_name VARCHAR(30),
+		last_name VARCHAR(30),
+		role_id INT, 
+		manager_id INT,
+		FOREIGN KEY(role_id) REFERENCES roles(id),
+		FOREIGN KEY (manager_id) REFERENCES employee(id),
+		PRIMARY KEY (id)
 );
