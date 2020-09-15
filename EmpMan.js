bbe977790f94f8a7c9927dd01ae88dddbdb11f17
Joel Terrell <falconreid@hotmail.com>
2020-09-14 20:20:23 -0600

    corrected coding errors in add functions

--- EmpMan.js
+++ EmpMan.js
@@ -15,133 +15,155 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
   showLogo();
   employeeQuestions();
-  connection.end();
 });
 
 // Inquirer Prompts
-const employeeQuestions = () => {
-  inquirer.prompt({
-    message: "Welcome. Please Select An Option",
-    type: "list",
-    name: "choice",
-    choices: [
-      "Add Department",
-      "Add Role",
-      "Add Employee",
-      "View Department",
-      "View Role",
-      "View Employee",
-      "Update Employee",
-      "I Am Done"
-      //   "Delete Department",
-      //   "Delete Role",
-      //   "Delete Employee"
-    ] .then((answers) => {
-        switch (answers.choice) {
-            case "Add Department":
-            addDepartment();  
-            break;
-            case "Add Role":
-            addRole();
-            break; 
-            case "Add Employee":
-            addEmployee();
-            break; 
-            case "View Department":
-            viewDepartment();
-            break;
-            case "View Role":
-            viewRole();
-            break;
-            case "View Employee":
-            viewEmployee();
-            break;
-            case "Update Employee":
-            updateEmployee();
-            break;
-            case "I Am Done":
-                console.log("Transaction Completed! Exiting Employee Tracker");
-                if (err) throw err;
-            console.log(err);
-            break;
-        }),
-  });
-};
-
-// add department function
-const addDepartment = () => {
-    inquirer.prompt([
-        {
-            message: "What is the Department Name?",
-            type: "input",
-            name: "department"
-        }
-    ]).then(function(res) {
-        connection.query("INSERT INTO  department(name) VALUES(?)", [res.department], function(err) {
-            if(err) throw err;
-            console.log(`The Department ${res.department} has been created.`)
-            employeeQuestions();
-        })
+function employeeQuestions() {
+  inquirer
+    .prompt({
+      message: "Welcome. Please Select An Option",
+      type: "list",
+      name: "choice",
+      choices: [
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "View Department",
+        "View Role",
+        "View Employee",
+        "Update Employee",
+        "I Am Done",
+        //   "Delete Department",
+        //   "Delete Role",
+        //   "Delete Employee"
+      ],
     })
-};
-
-const addRole = () => {
-    inquirer.prompt([
-        {
-            message: "What is the Organizational Title?",
-            type: "input",
-            name: "title"
-        },
-        {
-            message: "What is the Salary for this Title?",
-            type: "input",
-            name: "salary"
-        },
-        {
-            message: "What is the Department ID for this Title?",
-            type: "input",
-            name: "department_id"
-        },
-    ]).then(function(res){
-        connection.query("INSERT INTO roles(name, title, department_id ) VALUES(?,?,?)", [res.title, res.salary, res.department_id], function(err) {
-            if(err) throw err;
-            console.log(`The Organizational Role of ${res.title} has been created.`)
-        } )
-    })
+    .then((answers) => {
+      switch (answers.choice) {
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "View Department":
+          viewDepartment();
+          break;
+        case "View Role":
+          viewRole();
+          break;
+        case "View Employee":
+          viewEmployee();
+          break;
+        case "Update Employee":
+          updateEmployee();
+          break;
+        case "I Am Done":
+          console.log("Transaction Completed! Exiting Employee Tracker");
+          if (err) throw err;
+          console.log(err);
+          break;
+          connection.end();
+      }
+    });
 }
 
-const addEmployee = () => {
-    inquirer.prompt([
-        {
-            message: "What is the Employee's First Name?",
-            type: "input",
-            name: "first_name"
-        },
-        {
-            message: "What is the Employee's Last Name?",
-            type: "input",
-            name: "last_name"
-        },
-        {
-            message: "What is the Employee's Role ID?",
-            type: "input",
-            name: "role_id"
-        },
-        {
-            message: "What is the Employee's Manager ID?",
-            type: "input",
-            name: "manager_id"
-        },
-    ]).then(function(res){
-        connection.query("INSERT INTO employee(first_name, last_name, manager_id, role_id ) VALUES(?,?,?,?)", [res.first_name, res.last_name, res.role_id, res.manager_id], function(err) {
-            if(err) throw err;
-            console.log(`The employee ${res.first_name} ${res.last_name} has been added.`)
-        } )
-    })
-};
-        
+// add department function
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Department Name?",
+        type: "input",
+        name: "dept_name",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO  department(dept_name) VALUES(?)",
+        [res.dept_name],
+        function (err) {
+          if (err) throw err;
+          console.log(`The Department ${res.dept_name} has been created.`);
+          employeeQuestions();
+        }
+      );
+    });
+}
 
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Organizational Title?",
+        type: "input",
+        name: "title",
+      },
+      {
+        message: "What is the Salary for this Title?",
+        type: "input",
+        name: "salary",
+      },
+      {
+        message: "What is the Department ID for this Title?",
+        type: "input",
+        name: "department_id",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO roles(id, title, salary, department_id ) VALUES(?,?,?,?)",
+        [res.id, res.title, res.salary, res.department_id],
+        function (err) {
+          if (err) throw err;
+          console.log(
+            `The Organizational Role of ${res.title} has been created.`
+          );
+        }
+      );
+    });
+}
 
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Employee's First Name?",
+        type: "input",
+        name: "first_name",
+      },
+      {
+        message: "What is the Employee's Last Name?",
+        type: "input",
+        name: "last_name",
+      },
+      {
+        message: "What is the Employee's Role ID?",
+        type: "input",
+        name: "role_id",
+      },
+      {
+        message: "What is the Employee's Manager ID?",
+        type: "input",
+        name: "manager_id",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO employee(first_name, last_name, manager_id, role_id ) VALUES(?,?,?,?)",
+        [res.first_name, res.last_name, res.role_id, res.manager_id],
+        function (err) {
+          if (err) throw err;
+          console.log(
+            `The employee ${res.first_name} ${res.last_name} has been added.`
+          );
+        }
+      );
+    });
+}
 
 // fancy intro logo (for ascii)
 const showLogo = () => {
