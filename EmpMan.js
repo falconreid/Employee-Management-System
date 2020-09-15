eae34dda17fab1e17f139002e30f8f164628dc73
Joel Terrell <falconreid@hotmail.com>
2020-09-14 19:37:50 -0600

    add dept, role, employee functions added

--- EmpMan.js
+++ EmpMan.js
@@ -60,14 +60,90 @@ const employeeQuestions = () => {
             updateEmployee();
             break;
             case "I Am Done":
-                console.log("Transaction Completed!");
+                console.log("Transaction Completed! Exiting Employee Tracker");
                 if (err) throw err;
             console.log(err);
             break;
-        });
+        }),
   });
 };
 
+// add department function
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            message: "What is the Department Name?",
+            type: "input",
+            name: "department"
+        }
+    ]).then(function(res) {
+        connection.query("INSERT INTO  department(name) VALUES(?)", [res.department], function(err) {
+            if(err) throw err;
+            console.log(`The Department ${res.department} has been created.`)
+            employeeQuestions();
+        })
+    })
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            message: "What is the Organizational Title?",
+            type: "input",
+            name: "title"
+        },
+        {
+            message: "What is the Salary for this Title?",
+            type: "input",
+            name: "salary"
+        },
+        {
+            message: "What is the Department ID for this Title?",
+            type: "input",
+            name: "department_id"
+        },
+    ]).then(function(res){
+        connection.query("INSERT INTO roles(name, title, department_id ) VALUES(?,?,?)", [res.title, res.salary, res.department_id], function(err) {
+            if(err) throw err;
+            console.log(`The Organizational Role of ${res.title} has been created.`)
+        } )
+    })
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            message: "What is the Employee's First Name?",
+            type: "input",
+            name: "first_name"
+        },
+        {
+            message: "What is the Employee's Last Name?",
+            type: "input",
+            name: "last_name"
+        },
+        {
+            message: "What is the Employee's Role ID?",
+            type: "input",
+            name: "role_id"
+        },
+        {
+            message: "What is the Employee's Manager ID?",
+            type: "input",
+            name: "manager_id"
+        },
+    ]).then(function(res){
+        connection.query("INSERT INTO employee(first_name, last_name, manager_id, role_id ) VALUES(?,?,?,?)", [res.first_name, res.last_name, res.role_id, res.manager_id], function(err) {
+            if(err) throw err;
+            console.log(`The employee ${res.first_name} ${res.last_name} has been added.`)
+        } )
+    })
+};
+        
+
+
+
+// fancy intro logo (for ascii)
 const showLogo = () => {
   console.log("╔═╗╔╦╗╔═╗╦  ╔═╗╦ ╦╔═╗╔═╗");
   console.log("║╣ ║║║╠═╝║  ║ ║╚╦╝║╣ ║╣");
--- schema.sql
+++ schema.sql
@@ -6,14 +6,14 @@ USE employeeDB;
 
 CREATE TABLE department
 (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL,
     dept_name VARCHAR(30),
     PRIMARY KEY(id)
 );
 
     CREATE TABLE roles
     (
-        id INT NOT NULL AUTO_INCREMENT,
+        id INT NOT NULL,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
@@ -23,7 +23,7 @@ CREATE TABLE department
 
         CREATE TABLE employee
         (
-            id INT NOT NULL AUTO_INCREMENT,
+            id INT NOT NULL ,
             first_name VARCHAR(30),
             last_name VARCHAR(30),
             role_id INT,
